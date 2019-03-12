e8d9c8ef4f77b9037dcdcab7676466c64aa79c4f
Administrator <25145168@qq.com>
2019-03-12 23:05:16 +0800

    tabs

---
+++ src/pages/ui/tabs.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {Tabs, Card, Icon} from "antd";
+
+const TabPane = Tabs.TabPane;
+
+class Tab extends Component {
+    handleCallback = (key) => {
+        console.log(key)
+    };
+
+    componentWillMount() {
+        const panes = [
+            {
+                title: 'tab1',
+                content: 'tab1-content',
+                key: 1
+            }, {
+                title: 'tab2',
+                content: 'tab2-content',
+                key: 2
+            }, {
+                title: 'tab3',
+                content: 'tab3-content',
+                key: 3
+            },
+        ];
+        this.setState({
+            panes
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Card title={"Tab页签"}>
+                    <Tabs onTabClick={this.handleCallback}>
+                        <TabPane tab={<span><Icon type={"plus"}/>选项卡一 </span>} key="1">
+                            欢迎学习React
+                        </TabPane>
+                        <TabPane tab="Tab 2" key="2">超级无敌大帅哥</TabPane>
+                        <TabPane tab="Tab 3" key="3">这里可以嵌入其他控件</TabPane>
+                    </Tabs>
+                </Card>
+                <Card title={"动态Tab页签"}>
+                    <Tabs onTabClick={this.handleCallback}>
+                        {
+                            this.state.panes.map(panel => {
+                                return <TabPane tab={panel.title} key={panel.key}>
+                                    {panel.content}
+                                </TabPane>
+                            })
+                        }
+                    </Tabs>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default Tab;
--- src/router.js
+++ src/router.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
 import App from "./App";
 import Admin from "./admin";
+import Home from "./pages/home";
 import Login from "./pages/login";
 import Buttons from "./pages/ui/buttons";
 import Modals from './pages/ui/modals';
-import Home from "./pages/home";
+import Tabs from './pages/ui/tabs';
 import NoMath from "./pages/nomatch";
 
 export default class Router extends React.Component {
@@ -13,16 +14,19 @@ export default class Router extends React.Component {
         return (
             <HashRouter>
                 <App>
-                    <Redirect exact path={"/"} to="/admin/home" />
-                    <Route path={"/login"} component={Login}/>
-                    <Route path={"/admin"} render={() => <Admin>
-                        <Switch>
-                            <Route path={"/admin/home"} component={Home}/>
-                            <Route path={"/admin/ui/buttons"} component={Buttons}/>
-                            <Route path={"/admin/ui/modals"} component={Modals}/>
-                            <Route component={NoMath}/>
-                        </Switch>
-                    </Admin>}/>
+                    <Switch>
+                        <Route path={"/login"} component={Login}/>
+                        <Route path={"/admin"} render={() => <Admin>
+                            <Switch>
+                                <Route path={"/admin/home"} component={Home}/>
+                                <Route path={"/admin/ui/buttons"} component={Buttons}/>
+                                <Route path={"/admin/ui/modals"} component={Modals}/>
+                                <Route path={"/admin/ui/tabs"} component={Tabs}/>
+                                <Route component={NoMath}/>
+                            </Switch>
+                        </Admin>}/>
+                        <Redirect exact path={"/"} to="/admin/home"/>
+                    </Switch>
                 </App>
             </HashRouter>
         )
